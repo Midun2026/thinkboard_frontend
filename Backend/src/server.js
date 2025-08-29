fb135ac3ac2e2a7bc84577757aec2e2f38448ba5
Midun2026 <midunkumar.h2022ece@sece.ac.in>
2025-08-29 18:54:48 +0530

    third

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -31,7 +31,6 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -39,6 +38,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import Navbar from "../components/Navbar";
-import RateLimitedUI from "../components/RateLimitedUI";
+import Navbar from "../Components/Navbar";
+import RateLimitedUI from "../Components/RateLimitedUI";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
-import NoteCard from "../components/NoteCard";
-import NotesNotFound from "../components/NotesNotFound";
+import NoteCard from "../Components/NoteCard";
+import NotesNotFound from "../Components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
